4bd242f054433ba37671c2f1d95f97ecedf5356d
goodmeow <harunbam3@gmail.com>
2021-03-14 06:51:36 +0700

    apollopro: add to official devices

--- nad_apollopro.mk
+++ nad_apollopro.mk
@@ -11,9 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollopro device
 $(call inherit-product, device/xiaomi/apollopro/device.mk)
 
-# Inherit some common nad stuff.
-$(call inherit-product, vendor/nad/config/common_full_phone.mk)
+# Inherit some common NusantaraProject stuff.
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
+TARGET_USES_BLUR := true
+USE_PIXEL_CHARGING := true
+NAD_BUILD_TYPE := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+USE_GAPPS ?= true
+USE_AOSP_CLOCK := true
+
+# Product
 PRODUCT_NAME := nad_apollopro
 PRODUCT_DEVICE := apollopro
 PRODUCT_MANUFACTURER := Xiaomi
