a42c9cd3bf5580c74d1de9ae97f2a8543f391a36
goodmeow <goodmeow@nusantaradevs.org>
2021-04-12 23:15:10 +0700

    apollo: init dotos

--- dot_apollo.mk
+++ dot_apollo.mk
@@ -11,13 +11,31 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common dotProject stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := lineage_apollo
+# blur
+TARGET_SUPPORTS_BLUR := true
+
+# bootanim
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# gapps
+WITH_GAPPS ?= true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Face unlock
+TARGET_USES_FACE_UNLOCK := true
+
+# dotOs
+DOT_OFFICIAL := false
+
+# Product
+PRODUCT_NAME := dot_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Xiaomi Mi 10T
+PRODUCT_MODEL ?= Xiaomi Mi 10T Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
