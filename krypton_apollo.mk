2d413967bc6fdbf16356fb8aac0f0076ea4771b4
goodmeow <harunbam3@gmail.com>
2022-05-19 22:34:43 +0530

    apollo: init Krypton AOSP

--- krypton_apollo.mk
+++ krypton_apollo.mk
@@ -11,19 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common NusantaraProject stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-
-TARGET_USES_BLUR := true
-TARGET_SUPPORTS_QUICK_TAP := true
-USE_PIXEL_CHARGING := true
-NAD_BUILD_TYPE ?= UNOFFICIAL
-TARGET_BOOT_ANIMATION_RES := 1080
-USE_GAPPS ?= true
-USE_AOSP_CLOCK := true
+# Inherit some common krypton AOSP stuff.
+$(call inherit-product, vendor/krypton/config/common_full_phone.mk)
+TARGET_SUPPORTS_64_BIT_APPS := true
 
 # Product
-PRODUCT_NAME := nad_apollo
+PRODUCT_NAME := krypton_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
